99289d62a1eec0f0ab849399ef8eb392e090845c
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2025-08-26 02:44:02 +0530

    Learning about console.log(fixed keyword) and some basic random variables

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -164,54 +164,77 @@ console.log("!(k > l)=" , !(8 > 9)); */
 
 // Conditional Statements = if, else, else if, switch
 
-let complexion = "fair";
-if (complexion === "fair")
-{console.log("In summers you'll get tanned")};
+// let complexion = "fair";
+// if (complexion === "fair")
+// {console.log("In summers you'll get tanned")};
 
 
 
-{let complexion = "fair"; //black
-if (complexion === "fair")
-{console.log("In summers you'll get tanned")}
+// {let complexion = "fair"; //black
+// if (complexion === "fair")
+// {console.log("In summers you'll get tanned")}
 
-else 
-{console.log("No need to worry")}};
-
-
-
-{let complexion = "fair";
-    if (complexion ==="fair"){
-        console.log("its bad for ur skin");
-    }
-    else if (complexion === "black"){
-        console.log("its good for ur skin");
-    }
-    else if (complexion === "brown"){
-        console.log("its little bit ok for ur skin");
-    }
-    else 
-        {console.log("you're a great person");}
-    }
-
-
-    // Ternary operator 
-
-    let age= 25;
-    let output= (age>18? "you'll get a license" : "you'll not get any license");
-    {console.log(output);}
-
-
-    let grade = ("A","B");
-    let result = (grade===("A","B")? "You're pass" : "You're fail");
-    {console.log(result);}
+// else 
+// {console.log("No need to worry")}};
 
 
 
-    //Switch statement
+// {let complexion = "fair";
+//     if (complexion ==="fair"){
+//         console.log("its bad for ur skin");
+//     }
+//     else if (complexion === "black"){
+//         console.log("its good for ur skin");
+//     }
+//     else if (complexion === "brown"){
+//         console.log("its little bit ok for ur skin");
+//     }
+//     else 
+//         {console.log("you're a great person");}
+//     }
 
-    // console.log("RESTARTING THE JS LEARNING JOURNEY");
 
-    console.log("hello world");
+//     // Ternary operator 
+
+//     let age= 25;
+//     let output= (age>18? "you'll get a license" : "you'll not get any license");
+//     {console.log(output);}
+
+
+//     let grade = ("A","B");
+//     let result = (grade===("A","B")? "You're pass" : "You're fail");
+//     {console.log(result);}
+
+
+
+//     //Switch statement
+
+//     // console.log("RESTARTING THE JS LEARNING JOURNEY");
+
+//     console.log("hello world");
+
+
+
+
+// STARTING JS JOUNRNEY FROM TODAY NOW
+
+
+console.log ("I love learning javascript");
+console.log("Javascript is a beginner friendly language");
+
+// Variables:-
+fullname = ("Abhishek Chaudhary");
+Age = (22);
+Pencil =("Natraj");
+followers = (700);
+qualification =( "B.Tech");
+
+console.log(fullname);
+console.log(Age);
+console.log("Pencil");
+console.log (followers);
+console.log(qualification);
+
 
 
 
