1581ef67e89c37f500b83875549bd515add90694
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2025-12-25 20:05:26 +0530

    Just write a basic interview question of js

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1,317 +1,116 @@
-// From now learning javascript//
-
-/* console.log("hello world");
-console.log("Wha's your name");
-console.log("whatsup");
-console.log("whats going on");
-console.log("wow");
-
-
-// Variables //
-
-// Variables are containers for storing data values. In JavaScript, there are three ways to declare variables: var, let, and const.
-
-Name= ("Abhishek Chaudhary");
-Age= (20);
-Complextion= ("Fair");
-isright = true;
-console.log(Name);
-console.log(Age);
-
-
-let ages= (86);
-Name= ("Vicky");
-complextion= ("Fair");
-is_alive= ("true");
-is_studying= ("true");
-console.log(is_alive,is_studying);
-
-const pi= 3.14;
-const name= ("Abhishek Chaudhary");
-const age= (20);
-
-console.log(pi,name,age);
-
-
-
-{
-    let a= 5;
-    b= 8;
-    console.log(a-b);
-}
-{a= 6+1;
-    console.log(a)
-}
-
-// Datatypes//
-// In JavaScript, there are two types of data types: primitive and non-primitive. Primitive data types include string, number, boolean, null, undefined, and symbol. Non-primitive data types include objects and arrays.//
-
-
-let price= 999;
-ispass= true;
-console.log(price , ispass);
-
-
-const Student={
-    Name: "Abhishek Chaudhary",
-    age: 20,
-    isstudy : true,
-    state : "Delhi",
-    marks: "70%",
-}
-console.log(Student["Name"],age);
-console.log(Student.marks)
-
-Student ["age"] = Student ["age"] - 5;
-console.log(Student["age"]);
-
-Student["state"]= "Bihar";
-console.log(Student["state"]);
-
-
-
-// Practice question=1
-
-let pen ={
-    Name: "Butterflow blue ball-pen",
-    ratings: 4.5,
-    isdeal: true,
-    price: 10,
-    offer: 2,
-}
-console.log(pen.ratings);
-
-
-// Practice question= //
-
-let profile={
-    username: "Abhishek Chaudhary",
-    Name:"Abhishek",
-    followers:0,
-    following:0,
-    isverified: false,
-
-}
-
-console.log(profile["username"],profile["Name"],profile["following"]); 
-
-
-
-// Operators in js
-
-// Arithmetic Operators = +, -, *, /. %, **
-
-let a= 85;
-let b= 55;
-let c= a+b;
-
-console.log(a+b);
-console.log("a-b=", a-b);
-console.log(a*b);
-console.log("a/b=", a/b);
-console.log(a%b);
-console.log("a**b=", a**b);
-
-
-// Urinary Operators = ++, --, 
-
-let d= 5;
-let e= 10;
-let f= d+e;
-console.log("d+e=", ++f);
-console.log(d--);
-console.log(d);
-
-// Assignment Operators : =, +=, -=, *=, /=, %=, **=
-
-
-let g= 5;
-let h= 9;
-console.log(g += h); 
-console.log(h -= g);
-console.log(g *= h);
-console.log(h /= g);
-console.log(g %= h);
-console.log(h**= g);
-
-
-// Comparison Operators :  ==, !=, ===, !==, >, >=, <, <=
-
-
-let i= 8;
-let j= 9;
-console.log(i == j); 
-console.log(j != i);
-console.log(i === j);
-console.log(j !== i);
-console.log(i > j);
-console.log(j >= i);
-console.log(i < j);
-console.log(j <= i);
-
-
-// Logical Operators = &&, ||, !
-
-
-let k= 8;
-let l= 9;
-let cond1= (k < l);
-let cond2= (k != l);
-console.log("cond1 && cond2=", k < l && k != l); 
-console.log("cond1 || cond2=", k < l || k != l);
-console.log("!(k > l)=" , !(8 > 9)); */
-
-
-// Conditional Statements = if, else, else if, switch
-
-// let complexion = "fair";
-// if (complexion === "fair")
-// {console.log("In summers you'll get tanned")};
-
-
-
-// {let complexion = "fair"; //black
-// if (complexion === "fair")
-// {console.log("In summers you'll get tanned")}
-
-// else 
-// {console.log("No need to worry")}};
-
-
-
-// {let complexion = "fair";
-//     if (complexion ==="fair"){
-//         console.log("its bad for ur skin");
-//     }
-//     else if (complexion === "black"){
-//         console.log("its good for ur skin");
-//     }
-//     else if (complexion === "brown"){
-//         console.log("its little bit ok for ur skin");
-//     }
-//     else 
-//         {console.log("you're a great person");}
-//     }
-
-
-//     // Ternary operator 
-
-//     let age= 25;
-//     let output= (age>18? "you'll get a license" : "you'll not get any license");
-//     {console.log(output);}
-
-
-//     let grade = ("A","B");
-//     let result = (grade===("A","B")? "You're pass" : "You're fail");
-//     {console.log(result);}
-
-
-
-//     //Switch statement
-
-//     // console.log("RESTARTING THE JS LEARNING JOURNEY");
-
-//     console.log("hello world");
-
-
-
-
 // STARTING JS JOUNRNEY FROM TODAY NOW
 
 
-console.log ("I love learning javascript");
-console.log("Javascript is a beginner friendly language");
+// console.log ("I love learning javascript");
+// console.log("Javascript is a beginner friendly language");
 
-// Variables:-
-fullname = ("Abhishek Chaudhary");
-Age = (22);
-Pencil =("Natraj");
-followers = (700);
-qualification =( "B.Tech");
-people = "twenty";
+// // Variables:-
+// fullname = ("Abhishek Chaudhary");
+// Age = (22);
+// Pencil =("Natraj");
+// followers = (700);
+// qualification =( "B.Tech");
+// people = "twenty";
 
-console.log(fullname);
-console.log(Age);
-console.log("Pencil");
-console.log (followers);
-console.log(qualification);
-console.log(people);
-
-
-// Variables- let, const 
-
-let fruit = ("Orange");
-let Fruit = ("Orange");
-let _Name = "Abhishek Chaudhary";
-let Ages = (22);
-console.log(fruit);
-console.log(Fruit);
-console.log(_Name);
-console.log(Age);
+// console.log(fullname);
+// console.log(Age);
+// console.log("Pencil");
+// console.log (followers);
+// console.log(qualification);
+// console.log(people);
 
 
+// // Variables- let, const 
 
-const students = "twenty";
-const student = "twenty";
-const price = 18400;
-const prices = 20000;
-console.log(students);
-console.log(student);
-console.log(price);
-console.log(prices);
-
-
-// Datatypes-
-
-1- Number
-2- String
-3- Boolean
-4- null
-5- undefined
-6- BigInt
-7- Symbol
-
-let marks= 90;
-const number1= 45;
-console.log(marks);
-console.log(number1);
-
-let rupees= " Twenty five thousand";
-const rupees1= " twenty five thousand";
-console.log(rupees);
-console.log(rupees1);
-
-const he_studying= (true);
-let she_studying= (false);
-console.log(he_studying);
-console.log(she_studying);
-
-let jai=null;
-console.log(jai);
-
-const vibgyor= undefined;
-console.log(vibgyor);
-
-let my_number= BigInt(1234567890);
-console.log(my_number);
-
-const sign= Symbol("Hello World");
-console.log(sign);
+// let fruit = ("Orange");
+// let Fruit = ("Orange");
+// let _Name = "Abhishek Chaudhary";
+// let Ages = (22);
+// console.log(fruit);
+// console.log(Fruit);
+// console.log(_Name);
+// console.log(Age);
 
 
-// Practise Question 
+
+// const students = "twenty";
+// const student = "twenty";
+// const price = 18400;
+// const prices = 20000;
+// console.log(students);
+// console.log(student);
+// console.log(price);
+// console.log(prices);
 
 
-let pen =  {
-    pen_name: "Butterflow",
-    price: 10,
-    reviews: 4.8,
-    is_deal: true
+// // Datatypes-
+
+// 1- Number
+// 2- String
+// 3- Boolean
+// 4- null
+// 5- undefined
+// 6- BigInt
+// 7- Symbol
+
+// let marks= 90;
+// const number1= 45;
+// console.log(marks);
+// console.log(number1);
+
+// let rupees= " Twenty five thousand";
+// const rupees1= " twenty five thousand";
+// console.log(rupees);
+// console.log(rupees1);
+
+// const he_studying= (true);
+// let she_studying= (false);
+// console.log(he_studying);
+// console.log(she_studying);
+
+// let jai=null;
+// console.log(jai);
+
+// const vibgyor= undefined;
+// console.log(vibgyor);
+
+// let my_number= BigInt(1234567890);
+// console.log(my_number);
+
+// const sign= Symbol("Hello World");
+// console.log(sign);
+
+
+// // Practice Question 
+
+
+// let pen =  {
+//     pen_name: "Butterflow",
+//     price: 10,
+//     reviews: 4.8,
+//     is_deal: true
+// };
+
+// console.log(pen);
+
+// pen_name1 ="Pilot";
+
+// console.log(pen_name1);
+
+
+const number = {
+    a : 10,
+    b : 20,
+    Sum() {
+        return this.a + this.b;
+}
 };
+console.log(number.Sum());
 
-pen_name1 ="Pilot";
 
-console.log(pen_name1);
+
+
+
 
 
 
