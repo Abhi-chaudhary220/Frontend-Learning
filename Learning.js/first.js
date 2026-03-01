029cadc5b7d56ad7fc7ad5aec5bf0cbd8c332629
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-03-01 23:46:37 +0530

    Some handsom practicee on variables

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1,113 +1,34 @@
-// STARTING JS JOUNRNEY FROM TODAY NOW
+// 
+
+// VARIABLES :-
+
+// var Name = "ABHISHEK";
+// Name = "VICKY";
+// Name = "DEEPU";
+// console.log(Name);
+
+// VAR VARIABLE ME VARIABLE NAME AND VALUE DONO HE UNLIMITED TIMES UPDATE KRI JA SKTI H JISSE CONFUSION HO SKTI H AND  VAR VARIABLE FUNCTION K ANDR HE EXECUTE HOTA H BUT OUTSIDE THE FUNCTION EXECUTE NHI HOTA H.
 
 
-// console.log ("I love learning javascript");
-// console.log("Javascript is a beginner friendly language");
-
-// // Variables:-
-// fullname = ("Abhishek Chaudhary");
-// Age = (22);
-// Pencil =("Natraj");
-// followers = (700);
-// qualification =( "B.Tech");
-// people = "twenty";
-
-// console.log(fullname);
-// console.log(Age);
-// console.log("Pencil");
-// console.log (followers);
-// console.log(qualification);
-// console.log(people);
-
-
-// // Variables- let, const 
-
-// let fruit = ("Orange");
-// let Fruit = ("Orange");
-// let _Name = "Abhishek Chaudhary";
-// let Ages = (22);
-// console.log(fruit);
-// console.log(Fruit);
-// console.log(_Name);
+// let Age = 20;
+// Age= 21;
 // console.log(Age);
 
+// LET VARIABLE ME VARIABLE NAME CHANGE NHI KIA JA SKTA BUT VALUE UNLIMITED TIMES UPDATE KRI JA SKTI H.
 
 
-// const students = "twenty";
-// const student = "twenty";
-// const price = 18400;
-// const prices = 20000;
-// console.log(students);
-// console.log(student);
+// const price = 500;
+// price = 600 This will give an error because we cannot change variable name and value in const variable.
 // console.log(price);
-// console.log(prices);
 
 
-// // Datatypes-
+// VARIABLE NAMING RULES :- SIRF LETTERS,NUMBERS,UNDERSCROES,DOLLAR SIGN USE KIYE JA SKTE H.
 
-// 1- Number
-// 2- String
-// 3- Boolean
-// 4- null
-// 5- undefined
-// 6- BigInt
-// 7- Symbol
+// let My_name_1 = ("Abhiiii");
+// console.log(My_name_1);
 
-// let marks= 90;
-// const number1= 45;
-// console.log(marks);
-// console.log(number1);
-
-// let rupees= " Twenty five thousand";
-// const rupees1= " twenty five thousand";
-// console.log(rupees);
-// console.log(rupees1);
-
-// const he_studying= (true);
-// let she_studying= (false);
-// console.log(he_studying);
-// console.log(she_studying);
-
-// let jai=null;
-// console.log(jai);
-
-// const vibgyor= undefined;
-// console.log(vibgyor);
-
-// let my_number= BigInt(1234567890);
-// console.log(my_number);
-
-// const sign= Symbol("Hello World");
-// console.log(sign);
-
-
-// // Practice Question 
-
-
-// let pen =  {
-//     pen_name: "Butterflow",
-//     price: 10,
-//     reviews: 4.8,
-//     is_deal: true
-// };
-
-// console.log(pen);
-
-// pen_name1 ="Pilot";
-
-// console.log(pen_name1);
-
-
-const number = {
-    a : 10,
-    b : 20,
-    Sum() {
-        return this.a + this.b;
-}
-};
-console.log(number.Sum());
-
-// All of the above code is becoming comment now
+// const price$ = 500;
+// console.log (price$);
 
 
 
