8a83719df29e1f6729e60f7e8cb42cdbafcdb763
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-03-13 16:36:36 +0530

    Learn about block scope and object

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -3,8 +3,8 @@
 // VARIABLES :-
 
 // var Name = "ABHISHEK";
-// Name = "VICKY";
-// Name = "DEEPU";
+// var Name = "VICKY";
+// var Name = "DEEPU";
 // console.log(Name);
 
 // VAR VARIABLE ME VARIABLE NAME AND VALUE DONO HE UNLIMITED TIMES UPDATE KRI JA SKTI H JISSE CONFUSION HO SKTI H AND  VAR VARIABLE FUNCTION K ANDR HE EXECUTE HOTA H BUT OUTSIDE THE FUNCTION EXECUTE NHI HOTA H.
@@ -13,14 +13,14 @@
 // let Age = 20;
 // Age= 21;
 // console.log(Age);
-let price = 899;
-console.log(typeof price);
+// let price = 899;
+// console.log(typeof price);
 
 // LET VARIABLE ME VARIABLE NAME CHANGE NHI KIA JA SKTA BUT VALUE UNLIMITED TIMES UPDATE KRI JA SKTI H.
 
 
 // const price = 500;
-// price = 600 This will give an error because we cannot change variable name and value in const variable.
+//  price = 600; This will give an error because we cannot change variable name and value in const variable. so that's why we use const where there's rare chance of changing variable name amd value.
 // console.log(price);
 
 
@@ -29,8 +29,20 @@ console.log(typeof price);
 // let My_name_1 = ("Abhiiii");
 // console.log(My_name_1);
 
-// const price$ = 500;
-// console.log (price$);
+// const $price = 500;
+// console.log ($price);
+
+
+// FOR KNOWLEDGE PURPOSE - DEFAULT= CONST, IF THERE'S A CHANCE OF VALUE CHANGE THEN= LET, AVOID= VAR;
+
+// IMPORTANT:- variable aand values changed in const only when there'e a object.
+
+// const user = { Name: "Deepu" };
+// user.Name="Vicky";
+// console.log(user.Name);
+
+
+// NOW PRACTICING VARIABLES AND DATATYPES FROM GPT LEARNINGS:-
 
 
 
