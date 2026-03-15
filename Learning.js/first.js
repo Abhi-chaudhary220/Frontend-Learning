997ae426001565d88682f1bd25a969a402bccc01
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-03-16 02:03:49 +0530

    learned about symbol and bigint datatypes

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -37,12 +37,84 @@
 
 // IMPORTANT:- variable aand values changed in const only when there'e a object.
 
-// const user = { Name: "Deepu" };
+// const user = { Name: "Deepu", age: 20 };
 // user.Name="Vicky";
-// console.log(user.Name);
+// console.log(Object.values(user));
+// console.log(typeof user);
 
 
 // NOW PRACTICING VARIABLES AND DATATYPES FROM GPT LEARNINGS:-
+
+
+// const user= {
+//     Name: "Abhishek",
+//     Age: 20,
+//     is_Student: true
+// };
+// console.log(user["is_Student"]);
+
+
+// There is two options for accessing keys and values from an object:- DOT NOTATION (Object.key) , BRACKET NOTATION (Object["key"]) .
+
+
+
+// DATATYPES:-
+
+
+// SYMBOL :-
+
+
+// const id= Symbol("my_id");
+// const id_2= Symbol("my_id");
+// const user = {
+//     Name: "Abhishek",
+//     Age: 20,};
+
+//     user[id]=123;
+//     user[id_2]=456;
+
+//     console.log(user[id]);
+//     console.log(user[id_2]);
+        
+
+
+
+// const color= Symbol("color");
+// const user={Name: "Orange"};
+// user[color]="Orange";
+
+// const color_2= Symbol("color");
+// const user_2={Name: "Yellow"};
+// user_2[color_2]="Yellow";
+
+// console.log(user[color]);
+// console.log(user_2[color_2]);
+
+
+
+
+// const id= Symbol("id");
+// const obj = {
+//     Name: "Abhishek",
+//     Age: 20,
+//     [id]: 127
+//     };
+
+// const symbol= Object.getOwnPropertySymbols(obj);
+// console.log(obj[symbol[0]]);
+
+
+
+// BIGINT:-
+
+
+// const max= Number.MAX_SAFE_INTEGER;
+// console.log(max);
+
+// const adding_values_bigint= 9007199254740991n + 5n;
+// console.log(adding_values_bigint);
+
+
 
 
 
