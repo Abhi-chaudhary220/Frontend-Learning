b7e5f45bc62176a99967ceef011a87bca2cb5689
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-09 23:31:57 +0530

    practicing

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -880,6 +880,18 @@
 // QUESTION:-
 
 
+let elements = [250, 645, 300, 900, 50];
+let offer = 0;
+let result = 0;
+for(let items of elements){
+    offer = items / 10;
+    result = items -offer;
+    console.log(result);
+
+};
+
+
+
 
 
 
