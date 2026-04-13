91008132dcefdf4f4b0dd753caf4f933ae64971d
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-14 00:34:58 +0530

    learning array reduce method

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -970,11 +970,13 @@
 
 // Starting reduce :-
 
-// let arr3 = [1, 30, 40, 50, 60];
-// let result3 = arr3.reduce((output) => {
-//     return output + output ;
-// });
-// console.log(result3);
+let arr3 = [1, 3, 4, 5, 6, 7];
+let result3 = arr3.reduce((value1, value2) => {
+    return value1 + value2 ;
+});
+console.log(result3);
+
+
 
 
 
