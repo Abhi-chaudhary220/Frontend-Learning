45d01267025adaefcf8a7dfb51b3577ce6627823
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-13 23:33:19 +0530

    commenting

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -955,10 +955,10 @@
 
 // Starting array - Map, filter, reduce:-
 
-let arr = [10,20,5,30,4,50,2,70,8,90,3];
-let result = arr.map((a) => {
-    console.log(a*3);
-    });
+// let arr = [10,20,5,30,4,50,2,70,8,90,3];
+// let result = arr.map((a) => {
+//     console.log(a*3);
+//     });
     
 
 
