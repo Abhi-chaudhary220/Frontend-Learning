cb020aae36440422c3ef7e95ec6318e46ed9b425
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-17 23:46:59 +0530

    commenting

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1036,12 +1036,12 @@
 // };
 // Num(7,5);
 
-function division(a,b){
-    result = division(a/b);
-    return result;
-};
-division(4,2);
-console.log(division);
+// function division(a,b){
+//     result = division(a/b);
+//     return result;
+// };
+// division(4,2);
+// console.log(division);
 
 
 
