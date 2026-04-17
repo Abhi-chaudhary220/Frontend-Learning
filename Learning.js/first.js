aaa78400bcf5cd6b4e2ae0e6b1387c9bb7854a03
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-18 00:25:08 +0530

    fix error of division of function

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1036,12 +1036,12 @@
 // };
 // Num(7,5);
 
-// function division(a,b){
-//     result = division(a/b);
-//     return result;
-// };
-// division(4,2);
-// console.log(division
+function division(a,b){
+    result = (a/b);
+    return result;
+};
+division(8,2);
+console.log(result);
 
 
 // const num = (x,y)=>{
