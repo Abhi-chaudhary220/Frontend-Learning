df7791af51678850f3e2b2b2fd55eafa00f1ead9
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-24 02:07:36 +0530

    Writing description

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1205,6 +1205,9 @@
 // });
 // console.log(arr);
 
+yha console.log ko bahr se access isliye kr parha hu kyuki block joki global h usme map function ko maine ek variable me store kra dia h jisse usko main call kr skta hu block k bahr se bhi.
+
+
 
 
 
