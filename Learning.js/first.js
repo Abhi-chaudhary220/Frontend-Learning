ffda9dc45aead0eafbd62080c7e8b8a51d503e15
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-04-30 01:27:36 +0530

    learning by for.each method

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1319,7 +1319,13 @@
 // };
 
 
-
+const song = [...document.getElementsByClassName("bhojpuri")];
+song.forEach(function(value) {
+    value.style.color = "green";
+    console.dir(value);
+    return value;
+    
+});
 
 
 
