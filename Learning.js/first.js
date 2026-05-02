cff81bfaaae1d69b96b9897e04b800a8636e22a6
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-05-02 20:20:02 +0530

    commenting query method

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1363,13 +1363,13 @@
 // console.dir(element_by_tag);
 
 
-const paragraph_collection = document.querySelectorAll("p");
-paragraph_collection.forEach(function(value){
-    value.style.color = "blue";
-    value.style.fontWeight = "bold";
-    console.dir(value);
-    return value;
-});
+// const paragraph_collection = document.querySelectorAll("p");
+// paragraph_collection.forEach(function(value){
+//     value.style.color = "blue";
+//     value.style.fontWeight = "bold";
+//     console.dir(value);
+//     return value;
+// });
 
 
 
