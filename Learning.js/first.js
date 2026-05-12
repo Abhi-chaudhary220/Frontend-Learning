81bdcf822a932757f2f594917b61de5944d90569
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-05-12 23:21:09 +0530

    making comment

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1705,6 +1705,6 @@
 // arr.shift();
 // console.log(arr);
 
-const arr = [1,2,3,4,5,6,7,8,9,10];
-arr.unshift(2,3,4,);
-console.log(arr);
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+// arr.unshift(2,3,4,);
+// console.log(arr);
