e2e9893296800ce67d98f6f8b108dc2dd500f9d3
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-05-13 03:18:42 +0530

    toggle button by method -2

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -1752,4 +1752,18 @@
 
 
 
+let btn = document.querySelector("button");
+let mode = "light";
+btn.addEventListener("click", () => {
+    if (mode === "light") {
+        mode = "dark";
+        document.body.classList.add ("dark");
+        document.body.classList.remove ("light");
+    } else {
+        mode = "light";
+        document.body.classList.add ("light");
+        document.body.classList.remove ("dark");
+    }
+}); 
+
 
