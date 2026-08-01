33c55afdc32ea7233a577fe2360a4951919d7644
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-08-01 23:45:25 +0530

    practicing api end points

--- Learning.js/first.js
+++ Learning.js/first.js
@@ -2520,7 +2520,7 @@
 // Starting learning about APIs
 
 async function getFacts() {
-let URL = "https://api.astroworldmc.com/api/v1/versions";
+let URL = "https://api.astroworldmc.com/api/v1/achievements";
 let response = await fetch(URL);
 let data = await response.json();
 console.log(data); 
